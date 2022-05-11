75207539481311652e401ccd36f8a72dfd97699c
Evgen Sikora <arnika200@gmail.com>
2022-05-11 12:39:18 +0300

    still in process

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs')
 
+
 const port = 8080;
 
 const app = express();
@@ -12,18 +13,18 @@ app.get('/', function (request, response) {
     console.log (request.url);
 });
 
-const urlencodedParser = bodyParser.urlencoded({ extended: true })
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 
-app.post('/', urlencodedParser, function (request, response) {
-    if (!request.body) return response.sendStatus(400)
-    console.log(request.body)
-    response.send(`${request.body}`)
+app.post('/', function (request, response) {
+    if (!request.body) return response.sendStatus(400);
+    response.send(`${request.body}`);
+    console.log(request.body);
 });
 
 
 app.listen(8080, ()=>{
     console.log ('Server start on port '+`${port}`);
-})
-
+});
 
